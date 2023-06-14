051b0867efcb76b98e7a209c4bc579f5b1f5a0cb
João Vitor De Lima <88281625+joaolima16@users.noreply.github.com>
2023-06-14 19:25:07 -0300

    Add files via upload

---
+++ Controllers/Product.cs
@@ -0,0 +1,97 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Models;
+using api.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.AspNetCore.Authorization;
+
+namespace api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class Product : ControllerBase
+    {
+        private IProduct _IProduct;
+
+        public Product(IProduct productInterface) => _IProduct = productInterface;
+        
+        [HttpGet]
+        public IActionResult listProducts()
+        {
+            try
+            {
+                return Ok(_IProduct.products());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Ocorreu algum erro: " + ex.Message);
+            }
+
+        }
+        [Authorize]
+        [HttpGet("{Name?}")]
+        public IActionResult listProduct(string Name)
+        {
+
+            if (_IProduct.product(Name).Any())
+            {
+                
+                return Ok(_IProduct.product(Name));
+            }
+            else
+            {
+                return NotFound("Não foi possivel encontrar o produto");
+            }
+
+        }
+        [Authorize]
+        [HttpPost]
+        public ActionResult AddProduct([FromBody] ProductModel product)
+        {
+            try
+            {
+                _IProduct.addProduct(product);
+                return Ok(product);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Ocorreu algum erro: " + ex.Message);
+            }
+
+        }
+
+        [Authorize]
+        [HttpPut("{Name}")]
+        public IActionResult UpdateProduct(string Name, [FromBody] ProductModel product)
+        {
+            try
+            {
+                _IProduct.updateProduct(Name, product);
+                return Ok(product);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Ocorreu um erro: " + ex.Message);
+            }
+        }
+
+        [Authorize]
+        [HttpDelete("{Name}")]
+        public IActionResult deleteProduct(string Name)
+        {
+            try
+            {
+                _IProduct.deleteProduct(Name);
+                return Ok("Produto deletado");
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Ocorreu um erro:" + ex.Message);
+            }
+        }
+    }
+}
---
+++ Controllers/User.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Data;
+using api.Models;
+using api.Repository;
+using api.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class User : ControllerBase
+    {
+
+        private IUser _IUser;
+
+        public User(IUser iuser) => _IUser = iuser;
+
+
+
+        [HttpGet]
+        public IActionResult selectUser([FromBody] UserModel user)
+        {
+            var tokenGenerator = new Token();
+            if (_IUser.userExists(user) == true)
+            {
+                return Ok(tokenGenerator.GenerateToken(user.Email));
+            }
+            else
+            {
+                return BadRequest("Login invalido");
+            }
+        }
+        [HttpPost]
+        public IActionResult AddUser([FromBody] UserModel user)
+        {
+            try
+            {
+                _IUser.addUser(user);
+                return Ok(user);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpDelete("{id?}")]
+        public IActionResult deleteUser(int id)
+        {
+            try
+            {
+                _IUser.deleteUser(id);
+                return Ok("Usuário foi deletado");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return BadRequest("Ocorreu um erro ao deletar o usuário");
+                
+            }
+        }
+    }
+}
